da51692364aca368880faefb5e2bea9ad0535492
Me <81636147+shillminator@users.noreply.github.com>
2022-01-06 20:50:52 +0100

    added schema's

--- support/database/database_connection.py
+++ support/database/database_connection.py
@@ -1,6 +1,7 @@
 import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.schema import CreateSchema
 import time
 from decouple import config
 
@@ -15,6 +16,8 @@ class DatabaseSession:
 
         self.starttime_connection = time.time()
         self.engine = create_engine(self.DATABASE_URL,echo=False)
+
+
 
 
     def __enter__(self):
