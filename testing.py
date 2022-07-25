b5b27ff944ef66ae8404768d3f4db2fc645b575a
JoHey <jonas.heymans@hotmail.com>
2022-07-25 14:49:12 +0200

    updated requirements.txt

--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 from mainapp.microservice_supplier.api.supplier import EdcClient, BigbuyClient
 from mainapp.microservice_supplier.parsers.converter import EdcConverter, BigbuyConverter
 from scheduler import offer_update
+from mainapp.microservice_supplier.adapter.adapter import BigbuyAdapter, EdcAdapter
 from support.database.database import EdcDatabase,BigbuyDatabase
 from support.logger.logger import Logger
 
@@ -23,6 +24,8 @@ bibu.download()
 
 con = BigbuyConverter()
 con.initial_convert()
+
+ad = BigbuyAdapter
 
 db = BigbuyDatabase(connection_type='merge')
 db.add_to_db()
